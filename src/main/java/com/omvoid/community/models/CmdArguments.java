5cff5da32205ac1aca9d1c0f281cb21e81e8cb12
Kit <pinigins55@gmail.com>
2020-06-06 14:43:11 +0300

    Fix default result dir

--- src/main/java/com/omvoid/community/models/CmdArguments.java
+++ src/main/java/com/omvoid/community/models/CmdArguments.java
@@ -7,7 +7,7 @@ public class CmdArguments {
     private String inputFile = "input.txt";
     private String delimiter = ",";
     private String commentStartWith = "#";
-    private String outputDirectory = "result/";
+    private String outputDirectory = "result";
 
     @Override
     public String toString() {
